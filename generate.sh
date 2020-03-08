776d5249170bd824635ba48c61ba7907e37d640f
Roland Jülich <rolandjuelich@gmail.com>
2020-03-08 08:32:59 +0100

    fix script

--- generate.sh
+++ generate.sh
@@ -1,12 +1,12 @@
-#mkdir build
+mkdir build
 cd build
-#git clone https://github.com/jboss-fuse/wsdl2rest.git
-#cd wsdl2rest
-#git checkout master
-#mvn clean install -DskipTests
-#cd ..
-#unzip wsdl2rest/dist/target/wsdl2rest-dist-*.zip 
-#mv wsdl2rest-*SNAPSHOT wsdl2rest-generator
+git clone https://github.com/jboss-fuse/wsdl2rest.git
+cd wsdl2rest
+git checkout master
+mvn clean install -DskipTests
+cd ..
+unzip wsdl2rest/dist/target/wsdl2rest-dist-*.zip 
+mv wsdl2rest-*SNAPSHOT wsdl2rest-generator
 sh wsdl2rest-generator/bin/wsdl2rest.sh --out . --wsdl http://localhost:8080/ws/countries.wsdl
 cp -Rfv java/* ../src/main/java
 cp -fv camel/wsdl2rest-camel-context.xml ../src/main/resources/applicationContext.xml
